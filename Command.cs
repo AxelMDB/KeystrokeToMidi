603bfa917084718ef5d3c138bd258d671910308e
axeldamaso <axeldasperger@gmail.com>
2022-02-11 21:57:56 -0400

    Agregar archivos de proyecto.

---
+++ Command.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace KeystrokeToMidi
+{
+    public class Command : ICommand
+    {
+        Action<object> _execute;
+        Func<object, bool> _canExecute;
+        public Command(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute != null)
+            {
+                return _canExecute(parameter);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ MainWindowModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Controls;
+using Commons.Music.Midi;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace KeystrokeToMidi
+{
+    public class MainWindowModel : INotifyPropertyChanged
+    {
+        private string labelText;
+        private ICommand buttonCommand;
+        private IMidiAccess access;
+        private IMidiOutput output;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string LabelText
+        {
+            get { return labelText; }
+            set 
+            { 
+                labelText = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public ICommand ButtonCommand
+        {
+            get { return buttonCommand; }
+            private set
+            {
+                buttonCommand = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public MainWindowModel()
+        {
+            LabelText = "Hello";
+            ButtonCommand = new Command(onButton_Click, CanExecute);
+            access = MidiAccessManager.Default;
+            output = access.OpenOutputAsync(access.Outputs.Last().Id).Result;
+
+            //output.CloseAsync();
+        }
+        private bool CanExecute(object value)
+        {
+            return true;
+        }
+        private void onButton_Click(object obj)
+        {
+            LabelText = ":>";
+            output.Send(new byte[] {MidiEvent.Program, 1}, 0, 0, 0);
+        }
+
+        // This method is called by the Set accessor of each property.  
+        // The CallerMemberName attribute that is applied to the optional propertyName  
+        // parameter causes the property name of the caller to be substituted as an argument.  
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
